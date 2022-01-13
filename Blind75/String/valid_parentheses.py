83c8f6bfe7f789fa150545a4ab3ae594de22cf88
Xinyi Zhou <x375zhou@uwaterloo.ca>
2022-01-13 11:02:58 -0500

    leetcode 20

---
+++ Blind75/String/valid_parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        matched = {')': '(', '}': '{',']': '['}
+        stack = []
+        for char in s:
+            if char in ('(', '{', '['):
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                if matched[char] != stack[-1]:
+                    return False
+                stack.pop()
+                
+        return len(stack) == 0
