d9e09311ba3b70a0af99d5fc532f0027f3013834
Xinyi Zhou <x375zhou@uwaterloo.ca>
2022-01-17 09:49:05 -0500

    leetcode 377

--- Blind75/DynamicProgramming/word_break.py
+++ Blind75/DynamicProgramming/word_break.py
@@ -22,12 +22,13 @@ class Solution:
     """
     I don't think this is as readable tbh
     """
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+    def wordBreak2(self, s: str, wordDict: List[str]) -> bool:
         canBreak = [False]*(len(s)+1)
         canBreak[0] = True
         wordSet = set(wordDict)
         
         for i in range(1,len(s)+1):
+            #note that index i in canBreak maps to i-1 in s
             canBreak[i] = any(canBreak[j] and s[j:i] in wordSet for j in range(i))
                 
         return canBreak[-1]
