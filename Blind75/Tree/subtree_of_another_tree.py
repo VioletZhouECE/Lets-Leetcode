d6b84228c2e121887eff97aa81372272c125a99e
Xinyi Zhou <x375zhou@uwaterloo.ca>
2022-01-25 10:43:30 -0500

    leetcode 572

---
+++ Blind75/Tree/subtree_of_another_tree.py
@@ -0,0 +1,33 @@
+"""
+https://leetcode.com/problems/subtree-of-another-tree/
+
+DFS:
+root: n nodes, subRoot: m nodes
+Time complexity: O(m*n).
+Space complexity: O(height(m)+height(n)).
+O(height(n)) comes from isSubtree, O(height(m)) comes from isSameTree
+We need to store a "path" on stack (aka, the node and its sibling)
+
+BFS in theory have similar time and space complexity, but it runs faster for this question in practice. 
+Probably doing the searching level by level has an advantage.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+        if self.isSameTree(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        
+    def isSameTree(self, rootNode, subRootNode):
+        if not (rootNode and subRootNode):
+            return rootNode is subRootNode
+        if rootNode.val == subRootNode.val:
+            return self.isSameTree(rootNode.left, subRootNode.left) and self.isSameTree(rootNode.right, subRootNode.right)
+        return False
