27900d68bce9598ac3fe269ecb8df4d8ac6868aa
Xinyi Zhou <x375zhou@uwaterloo.ca>
2022-01-27 11:27:47 -0500

    update leetcode 105

--- Blind75/Tree/construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ Blind75/Tree/construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -6,7 +6,9 @@ For every subtree,
 Preorder traversal gives us root node, children nodes (left nodes + right nodes)
 Inorder traversal tells us how to partition the children nodes to left nodes | right nodes
 
-Time complexity: O(n). Space complexity: O(h) - same as tree traversal
+Time complexity: O(n^2). Space complexity: O(h) - same as tree traversal
+Best-case: T(n) = 2T(n/2) + O(n) -> T(n) = O(nlogn)
+Worst-case: T(n) = 2T(n-1) + O(n) -> T(n) = O(n^2)
 
 Takeaway: divide and conquer algo is very commonly used in tree problems. Just focus on one node, its left children, its right children at a time.
 
@@ -19,14 +21,25 @@ Takeaway: divide and conquer algo is very commonly used in tree problems. Just f
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        return self.buildTreeHelper(preorder, inorder)
-        
-    def buildTreeHelper(self, preorder, inorder):
         if not preorder:
             return None
         root = TreeNode(preorder[0], None, None)
         leftChildIndex = inorder.index(preorder[0])
-        root.left = self.buildTreeHelper(preorder[1:leftChildIndex+1], inorder[:leftChildIndex+1])
-        root.right = self.buildTreeHelper(preorder[leftChildIndex+1:], inorder[leftChildIndex+1:])
+        root.left = self.buildTree(preorder[1:leftChildIndex+1], inorder[:leftChildIndex+1])
+        root.right = self.buildTree(preorder[leftChildIndex+1:], inorder[leftChildIndex+1:])
+        return root
+
+"""
+A similar question: https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+"""
+
+class Solution2:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not inorder:
+            return None
+        root = TreeNode(postorder[-1], None, None)
+        numOfLeftChild = inorder.index(postorder[-1])
+        root.left = self.buildTree(inorder[:numOfLeftChild], postorder[:numOfLeftChild])
+        root.right = self.buildTree(inorder[numOfLeftChild+1:], postorder[numOfLeftChild:len(postorder)-1])
         return root
         
