3dec7f277c224438ff2c603b4ef5f8d10cb9e36a
Xinyi Zhou <x375zhou@uwaterloo.ca>
2022-02-09 11:14:12 -0500

    leetcode 11

---
+++ Blind75/Array/container_with_the_most_water.py
@@ -0,0 +1,40 @@
+"""
+https://leetcode.com/problems/container-with-most-water/
+
+Classic two pointer problem:
+
+Violet's O(n) solution
+Move left until curr.height > prev.height
+Move right until curr.height >= left.height
+
+Important note: Why do we never move the right pointer backward?
+If we move it backward, the right line is going to decrease in height and determines the amount of water the container can hold: 
+((height[right]*(right-left)) is going to be smaller than the previous solution we found at left-1 (height[right]*(right-left+1)) 
+since the distance between left and right is smaller.
+
+A cleaner O(n) solution:
+If height[left] < height[right]: move left
+Otherwise: move right
+
+Does essentially the same thing but I think this is more difficult to reason about ;)
+
+"""
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height)-1
+        maxWater = 0
+        
+        while left < right:
+            #move left until curr.height > prev.height
+            while left > 0 and height[left-1] > height[left] and left < right:
+                left += 1
+            #move right until curr.height >= left.height
+            while height[right] < height[left] and left < right:
+                maxWater = max(maxWater, height[right]*(right-left))
+                right -= 1
+            maxWater = max(maxWater, height[left]*(right-left))
+            #don't forget this!
+            left += 1
+            
+        return maxWater
