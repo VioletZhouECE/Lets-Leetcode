84f746bf3d1efa9131124f7707cac43f299b6245
Xinyi Zhou <x375zhou@uwaterloo.ca>
2022-03-05 16:45:09 -0500

    leetcode 740

---
+++ Blind75/DynamicProgramming/delete_and_earn.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/delete-and-earn/
+
+This question is very similar to House Robber.
+Basically, if you choose the current number, you cannot choose its neighbours
+The only difference is that each number may occur multiple times (this is why you need a hashmap for counting)
+"""
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        maxNum = max(nums)
+        dp = [0 for i in range(maxNum+1)]
+        numCount = Counter(nums)
+        dp[1] = numCount[1] if 1 in numCount else 0
+        
+        for i in range(2,maxNum+1):
+            if i in numCount:
+                dp[i] = max(numCount[i]*i+dp[i-2], dp[i-1])
+            else: 
+                dp[i] = dp[i-1]
+        
+        return dp[-1]
