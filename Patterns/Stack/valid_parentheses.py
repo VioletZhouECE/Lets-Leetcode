b451030e1fff5243d657bc2d580f5bf8cb7b9811
Xinyi Zhou <x375zhou@uwaterloo.ca>
2022-03-13 09:51:33 -0400

    leetcode 20

---
+++ Patterns/Stack/valid_parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {"(": ")", "[": "]", "{": "}"}
+        
+        for char in s:
+            if char in ("(", "[", "{"):
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                if pairs[stack.pop()] != char:
+                    return False
+        
+        return len(stack) == 0
