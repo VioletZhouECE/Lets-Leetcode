b65e19f750c55255fa2e25ae8f12a6742e4c720b
Xinyi Zhou <x375zhou@uwaterloo.ca>
2022-03-23 15:07:40 -0400

    leetcode 991

---
+++ Patterns/Greedy/broken_calculator.py
@@ -0,0 +1,25 @@
+"""
+https://leetcode.com/problems/broken-calculator/
+
+Why does this greedy algorithm work?
+If the number is even, maybe we should add 1*n to the number and then divide it by two. Is that going to make things better?
+No, if you divide it by 2, it will be even closer to the power of 2. We should only add 1 to the number when we have to (aka when it becomes odd)
+"""
+class Solution:
+    #recursion
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        if target <= startValue:
+            return startValue-target
+        if target%2 == 0:
+            return 1+self.brokenCalc(startValue, int(target/2))
+        return 1+self.brokenCalc(startValue, target+1)
+    
+    #iteration
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        steps = 0
+        while target > startValue:
+            if target%2 == 0: target //= 2
+            else: target += 1
+            steps += 1
+        return steps + (startValue-target)
+    
