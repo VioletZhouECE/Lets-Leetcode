ab7ee9bbf6efe59dcc887d05ec8848349a9e0bcf
Xinyi Zhou <x375zhou@uwaterloo.ca>
2022-05-02 10:36:28 -0400

    leetcode 905

---
+++ Patterns/TwoPointers/sort_array_by_parity.py
@@ -0,0 +1,13 @@
+"""
+https://leetcode.com/problems/sort-array-by-parity/
+
+Just a step in Quicksort :)
+"""
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        left, right = 0, len(nums)-1
+        while left < right:
+            while left < right and nums[left] %2 == 0: left+=1
+            while left < right and nums[right] %2 != 0: right-=1
+            nums[left], nums[right] = nums[right], nums[left]
+        return nums
